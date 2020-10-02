80b9ea9f6763b309c9cdbceb02ab27a5eb7be246
Gusmonh <zs17004042@estudiantes.uv.mx>
2020-10-02 02:33:01 -0500

    add leaflet maps

--- leaflet-example/src/app/map/map.component.ts
+++ leaflet-example/src/app/map/map.component.ts
@@ -4,28 +4,28 @@ import * as L from 'leaflet';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
   private map;
 
   constructor() { }
-  
-  ngOnInit(){
+
+  ngOnInit() {
     this.initMap();
   }
 
   private initMap(): void {
     this.map = L.map('map', {
       center: [ 39.8282, -98.5795 ],
-      zoom: 15
+      zoom: 3
     });
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-     maxZoom: 19,
-     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
-   
+
 tiles.addTo(this.map);
-  }
-  
+
+}
 }
